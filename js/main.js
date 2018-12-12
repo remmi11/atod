4bc5da36e84309152001bc75c59cdcbe9da72f2e
remmi11 <wtgeographer@gmail.com>
2018-12-12 08:12:09 -0600

    added main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function(){
     "use strict";
-   
-    function e() {
-     $(".nav_button").click(function() {
-      $("#burger").toggleClass("active-sandwich"), $("body").toggleClass("body-transform"), $(".navigation").toggleClass("navigation-active"), $(".menu-helper-close").toggleClass("menu-helper-toggle")
-     }), $(".menu-helper-close").click(function() {
-      $("body").toggleClass("body-transform"), $("#burger").toggleClass("active-sandwich"), $(".navigation").toggleClass("navigation-active"), $(this).toggleClass("menu-helper-toggle")
-     })
+
+    function e(){
+        $(".nav_button").click(function(){
+            $("#burger").toggleClass("active-sandwich"),$("body").toggleClass("body-transform"),$(".navigation").toggleClass("navigation-active"),$(".menu-helper-close").toggleClass("menu-helper-toggle")
+        }),$(".menu-helper-close").click(function(){
+            $("body").toggleClass("body-transform"),$("#burger").toggleClass("active-sandwich"),$(".navigation").toggleClass("navigation-active"),$(this).toggleClass("menu-helper-toggle")
+        })
     }
-   
-    // function o() {
-    //  $(".back-to-top a").click(function(e) {
-    //   e.preventDefault(), $("html,body").animate({
-    //    scrollTop: 0
-    //   }, 300)
-    //  })
-    //}
-   });
+    function o(){
+        $(".back-to-top a").click(function(e){
+            e.preventDefault(),$("html,body").animate({
+                scrollTop:0
+            },300)
+        })
+    }
+    e(),o()
+});
